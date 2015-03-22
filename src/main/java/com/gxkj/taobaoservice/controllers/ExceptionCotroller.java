bb839e0d47152e43407a684f303b414d98d49e36
01jiangwei01 <01jiangwei01@163.com>
2015-03-22 17:05:39 +0800

    500，404页面

--- src/main/java/com/gxkj/taobaoservice/controllers/ExceptionCotroller.java
+++ src/main/java/com/gxkj/taobaoservice/controllers/ExceptionCotroller.java
@@ -27,6 +27,19 @@ public class ExceptionCotroller {
 		modelMap.put("ex", ex);
 		return "error/"+errorcod;
 	}
+	@RequestMapping("/404")
+	public String path404(HttpServletRequest request,HttpServletResponse response,ModelMap modelMap){
+ 
+		Exception ex = (Exception) request.getAttribute("javax.servlet.error.exception");
+		modelMap.put("ex", ex);
+		return "error/404";
+	}
+	@RequestMapping("/500")
+	public String path500(HttpServletRequest request,HttpServletResponse response,ModelMap modelMap){
+		Exception ex = (Exception) request.getAttribute("javax.servlet.error.exception");
+		modelMap.put("ex", ex);
+		return "error/500";
+	}
 	@RequestMapping("/noauth")
 	public String noauth( HttpServletRequest request,HttpServletResponse response,ModelMap modelMap){
 		return "error/noauth";

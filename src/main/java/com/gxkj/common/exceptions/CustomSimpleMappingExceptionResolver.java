65e1f164cbc81ef86d198397de4d6403b1136165
01jiangwei01 <01jiangwei01@163.com>
2015-04-01 17:37:05 +0800

    logback 日志

--- src/main/java/com/gxkj/common/exceptions/CustomSimpleMappingExceptionResolver.java
+++ src/main/java/com/gxkj/common/exceptions/CustomSimpleMappingExceptionResolver.java
@@ -12,6 +12,8 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
@@ -23,9 +25,12 @@ import com.gxkj.taobaoservice.dto.EntityReturnData;
 
 public class CustomSimpleMappingExceptionResolver extends SimpleMappingExceptionResolver  {
 	
+	protected final  Logger logger = LoggerFactory.getLogger(getClass());   
+	
 	protected ModelAndView doResolveException(HttpServletRequest request,  
             HttpServletResponse response, Object handler, Exception ex) { 
 		 String viewName = determineViewName(ex, request); 
+		 
 		 if (viewName != null) {// JSP格式返回  
 	            if (!(request.getHeader("accept").indexOf("application/json") > -1 || (request  
 	                    .getHeader("X-Requested-With")!= null && request  
@@ -35,7 +40,17 @@ public class CustomSimpleMappingExceptionResolver extends SimpleMappingException
 	                // Only apply it if we're processing a top-level request.  
 	                Integer statusCode = determineStatusCode(request, viewName);  
 	                if (statusCode != null) {  
-	                    applyStatusCodeIfPossible(request, response, statusCode);  
+	                	if (!(ex instanceof  BusinessException)){
+	                		
+	                		StackTraceElement [] errorMsgs = ex.getStackTrace();
+	                		List<String> errorMsgsList = new ArrayList<String>();
+	                		for(int i=0,l=errorMsgs.length;i<l;i++){
+	                			errorMsgsList.add(String.format("%s%s%s\t\t", errorMsgs[i].getClassName(),errorMsgs[i].getLineNumber(),errorMsgs[i].getMethodName()));
+	                		}
+	                    	 logger.error("异常：{}",errorMsgsList.toArray());
+	                    }
+	                    applyStatusCodeIfPossible(request, response, statusCode);
+	                   
 	                }  
 	                return getModelAndView(viewName, ex, request);  
 	            } else {// JSON格式返回  
@@ -67,6 +82,15 @@ public class CustomSimpleMappingExceptionResolver extends SimpleMappingException
 	    				}else{
 	    					json.setErrortype("system");
 	    					json.setMsg(   "系统问题,请找技术人员解决");
+	    				 
+		                		
+	                		StackTraceElement [] errorMsgs = ex.getStackTrace();
+	                		List<String> errorMsgsList = new ArrayList<String>();
+	                		for(int i=0,l=errorMsgs.length;i<l;i++){
+	                			errorMsgsList.add(String.format("%s%s%s\t\t", errorMsgs[i].getClassName(),errorMsgs[i].getLineNumber(),errorMsgs[i].getMethodName()));
+	                		}
+	                    	 logger.error("异常：{}",errorMsgsList.toArray());
+		                    
 	    				}
 	                    if(json.getEntity() == null){
 	                    	json.setEntity("");
